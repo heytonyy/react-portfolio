d4964ddb8cd5d270dd179c404539f5f337c947c9
Tony Aiello <heytonyyy3@gmail.com>
2023-01-18 09:17:16 -0800

    updated package.json and resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import { Document, Page, pdfjs } from "react-pdf";
+
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
+import pdf from "../../assets/../assets/resume.pdf";
+
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -17,7 +22,19 @@ function ResumeNew() {
   return (
     <div>
       <Container fluid className="resume-section">
-        
+
+      <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download Resume
+          </Button>
+        </Row>
+
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
