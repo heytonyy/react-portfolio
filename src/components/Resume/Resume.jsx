facf86aecb4b72d196a6d39c921d8b9bae452ca7
Tony Aiello <heytonyyy3@gmail.com>
2023-01-18 08:54:35 -0800

    updated resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -5,7 +5,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "http://raw.githubusercontent.com/heytonyy/media-uploads/main/resume.pdf";
+  "http://raw.githubusercontent.com/heytonyy/react-portfolio/master/src/assets/resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
